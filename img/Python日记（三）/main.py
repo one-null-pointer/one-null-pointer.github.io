ff179c8e0a7ed64cdf3135c2ee103c6ec5b8719b
one-null-pointer <1344146002@qq.com>
2022-08-25 23:36:18 +0800

    Site updated: 2022-08-25 23:35:49

---
+++ img/Python日记（三）/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+liaoyue = int(input("请给撩月打分："))
+
+if liaoyue >= 90:
+    print("不敢当不敢当")
+elif liaoyue >= 80:
+    print ("还行还行")
+elif liaoyue >= 70:
+    print ("咋地，对我有意见啊")
+else:
+    print ("你干嘛~哎呦")
+
